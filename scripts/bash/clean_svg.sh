938c40eac5940afa3bfbf6ca6e3b5e3eea131c37
Seb <sebastien.thuillier38@gmail.com>
2022-03-25 10:43:02 +0000

    face

--- scripts/bash/clean_svg.sh
+++ scripts/bash/clean_svg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # The goal of this script is to clean all svgs files to prevents the clip_path conflits by adding names to the clip_paths
-for f in assets/accessories/eyes/*.svg
+for f in assets/accessories/face/*.svg
 # Loop trough all the svg files 
 do 
     echo "Cleaning the file: $f"
