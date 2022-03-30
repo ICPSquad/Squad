aa8765e0ea0b3424fb048793a7fea002db20bfc1
Seb <sebastien.thuillier38@gmail.com>
2022-03-30 22:01:47 +0100

    upload_file (avatar) finally done & tested

--- scripts/bash/upload_file.zsh
+++ scripts/bash/upload_file.zsh
@@ -2,45 +2,47 @@
 file=${1}
 canister=${2:-charlie}
 network=${3:-local}
-filename=$(echo $file | sed -E "s/.+\///")
+category=${4}
+tags_component=${5}
+tags_layer=${6}
+description=${7:-}
+
+filePath=$(echo $file | sed -E "s/.+\///")
+name=${filePath%%.*}
 extension="${file##*.}"
+contentType=$(file --mime-type -b $file)
+categoryCandid="variant {$category}"
 
-
-log_line="Uploading $file to $canister on $network."
-echo $log_line
-
-echo "$log_line ...Emptying buffer"
-dfx canister --network $network call $canister uploadClear 
-
-echo "$log_line ...Uploading"
-byteArray=$(od -An -v -tuC background-base.svg)
+byteArray=$(od -An -v -tuC $file)
 byteSize=0;
-variable="vec {"
+payload="vec {"
 for byte in ${byteArray[@]}; do
     byteSize=$((byteSize+1))
-    variable+="${byte};"
+    payload+="${byte};"
 done
-variable+="}"
-dfx canister --network $network call $canister upload "($variable)" 
+payload+="}"
+
+
+
+log_line="Uploading $file of category $category with name $name and size $(( $byteSize / 1024 )) kb to canister $canister on network $network."
+echo $log_line
+
+
+echo "$log_line ...Emptying buffer"
+dfx canister --network $network call $canister uploadClear >> upload_log.txt
+
+
+echo "$log_line ...Uploading file"
+dfx canister --network $network call $canister upload "($payload)" >> upload_log.txt
 
 echo "$log_line ...Finalizing"
 
-
-echo $mimeType
-
-meta="record{
-    \"name\" = \"$name\";\
-    \"description"\" = \"$description\";\
-    \"tags\" = $filename;\
-    \"category\" = $byteSize;\
+meta="record {
+    name = \"$name\";
+    tags = vec{ \"$tags_component\"; \"$tags_layer\" };
+    description = \"$description\";
+    category = $categoryCandid
 }"
-
-# dfx canister --network $network call $canister uploadFinalize "(
-#     record {\
-#         \"contentType\" = \"$mimeType\";\
-#         \"meta\" = \"$filename\";\
-#         \"filePath\" = $filename;\
-#     }\
-# )"
-
+finalizeArg="\"$contentType\",$meta,\"$name\""
+dfx canister --network $network call $canister uploadFinalize "($finalizeArg)" >> upload_log.txt
 echo "$log_line ...Done"
