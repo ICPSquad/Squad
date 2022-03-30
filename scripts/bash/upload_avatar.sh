5e5e6252cb86c287347df91e2c2fb3f31fc345d8
Seb <sebastien.thuillier38@gmail.com>
2022-03-30 08:49:37 +0100

    Change manifest & upload script (avatar)

---
+++ scripts/bash/upload_avatar.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+canister=${1:charlie}
+network=${2:local}
+
+#Confirm before deploying to mainnet
+if [[ $network != "local" ]]
+then
+    read -r -p "Do you confirm uploading to mainnet? [Y/n] " input
+        case $input in 
+            [yY][eE][sS]|[yY])
+                echo "Deploying to mainnet"
+                ;;
+            [nN][oO]|[nN])
+                echo "Aborting"
+                exit 0
+                ;;
+            *)
+                echo "Invalid input..."
+                exit 1
+                ;;
+    esac
+fi
+
+manifest="./assets/avatar/manifest-avatar.csv"
+[ ! -f $manifest ] && { echo "$manifest file not found"; exit 99; }
