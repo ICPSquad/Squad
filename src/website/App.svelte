2d7440da35ce1b71724b6c85fdda4deb2ef1b7a7
Andrew <andrewmsellen@gmail.com>
2022-04-24 13:58:26 +1000

    feat: faqs page

--- src/website/App.svelte
+++ src/website/App.svelte
@@ -2,10 +2,12 @@
   import { Router, Route } from "svelte-routing";
   import Home from "./src/pages/Home.svelte";
   import CreateAvatar from "./src/pages/CreateAvatar.svelte";
+  import Faqs from "./src/pages/Faqs.svelte";
 </script>
 
 <Router>
   <Route path="create-avatar"><CreateAvatar /></Route>
+  <Route path="faqs"><Faqs /></Route>
   <Route path="/"><Home /></Route>
 </Router>
 
