05ed1c93bdf6a753bbdb9901f1189c3dc38e7386
Andrew <andrewmsellen@gmail.com>
2022-05-04 10:49:16 +1000

    feat: add home bg image

--- src/website/src/pages/Home.svelte
+++ src/website/src/pages/Home.svelte
@@ -2,61 +2,70 @@
   import Header from "../components/Header.svelte";
   import Footer from "../components/Footer.svelte";
   import AvatarWall from "../components/AvatarWall.svelte";
-  import AvatarComponentsSvg from "../components/AvatarComponentsSvg.svelte";
+  import { Link } from "svelte-routing";
 </script>
 
-<Header />
-<main class="container">
-  <div class="hero-section">
-    <AvatarWall />
-    <div class="content">
-      <div class="squad-number">
-        <div><strong>5,238</strong> SQUAD MEMBERS</div>
-        <img id="avatar-collage" src="/avatar-collage.png" alt="Collage of ICP Squad Avatars" />
+<div class="background">
+  <Header />
+  <main class="container">
+    <div class="hero-section">
+      <AvatarWall />
+      <div class="content">
+        <div class="squad-number">
+          <div><strong>5,238</strong> SQUAD MEMBERS</div>
+          <img
+            id="avatar-collage"
+            src="/assets/avatar-collage.png"
+            alt="Collage of ICP Squad Avatars"
+          />
+        </div>
+        <h1>Join the squad</h1>
+        <p class="large">
+          Start by minting your own ICP Squad avatar NFT, then get rewarded with
+          NFTs, ICP and other prizes as you explore the Internet Computer.
+        </p>
+        <Link to="/create-avatar">
+          <button> Create Avatar → </button>
+        </Link>
+        <button class="secondary"> Learn More </button>
       </div>
-      <h1>Join the squad</h1>
-      <p class="large">Start by minting your own ICP Squad avatar NFT, then get rewarded with NFTs, ICP and other prizes as you explore the Internet Computer.</p>
-      <button> Create Avatar → </button>
-      <button class="secondary"> Learn More </button>
     </div>
-  </div>
-  <AvatarComponentsSvg />
-</main>
+  </main>
+</div>
 <Footer />
 
 <style lang="scss">
-  @use "./src/website/src/styles" as *;
-
+  @use "../styles" as *;
   main {
     --page-feature-color: #{$green};
     min-height: 100vh;
   }
-
   .hero-section {
     display: grid;
     grid-template-columns: 1fr 1fr;
     grid-column-gap: 40px;
   }
-
+  .background {
+    background-image: url("/assets/home-hero-background.png");
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
   .squad-number {
     display: flex;
     align-items: center;
-
     #avatar-collage {
       height: 50px;
       margin-left: 20px;
     }
   }
-
   h1 {
     text-align: left;
     margin-top: 40px;
   }
-
   p.large {
+    font-size: 1.41rem;
     margin-bottom: 50px;
   }
-
   button {
     margin-bottom: 20px;
   }
