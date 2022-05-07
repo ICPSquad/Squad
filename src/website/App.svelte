aa834fe41c560c5d65c502102906a1db26f55ad1
Andrew <andrewmsellen@gmail.com>
2022-05-07 16:41:32 +1000

    feat: add placeholders for all pages

--- src/website/App.svelte
+++ src/website/App.svelte
@@ -1,16 +1,24 @@
 <script>
   import { Router, Route } from "svelte-routing";
-  import Home from "./src/pages/Home.svelte";
-  import Faqs from "./src/pages/Faqs.svelte";
-  import CreateAvatar from "./src/pages/CreateAvatar.svelte";
-  // import Random from "./src/pages/Random.svelte";
+  import Home from "@src/pages/Home.svelte";
+  import Faqs from "@src/pages/Faqs.svelte";
+  import CreateAvatar from "@src/pages/CreateAvatar.svelte";
+  import Random from "@src/pages/Random.svelte";
+  import Legendary from "@src/pages/Legendary.svelte";
+  import Accessories from "@src/pages/Accessories.svelte";
+  import About from "@src/pages/About.svelte";
+  import Partners from "@src/pages/Partners.svelte";
 </script>
 
 <Router>
   <Route path="create-avatar"><CreateAvatar /></Route>
   <Route path="/"><Home /></Route>
   <Route path="faqs"><Faqs /></Route>
-  <!-- <Route path="random"><Random /></Route> -->
+  <Route path="legendary-avatars"><Legendary /></Route>
+  <Route path="accessories"><Accessories /></Route>
+  <Route path="about-us"><About /></Route>
+  <Route path="partners"><Partners /></Route>
+  <Route path="random"><Random /></Route>
 </Router>
 
 <style global lang="scss">
---
+++ src/website/src/pages/Partners.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Header from "@components/shared/Header.svelte";
+  import Footer from "@components/shared/Footer.svelte";
+  import Join from "@components/shared/Join.svelte";
+  import Newsletter from "@components/shared/Newsletter.svelte";
+</script>
+
+<Header />
+<div class="page-header">
+  <h1>Partners</h1>
+</div>
+<div class="container" />
+<Join />
+<Newsletter />
+<Footer />
+
+<style lang="scss">
+  @use "../styles" as *;
+
+  main,
+  h1 {
+    --page-feature-color: #{$yellow};
+  }
+</style>
